5e993aa2ae5fdfdd996f67ba083e62f862372289
annetran2602 <anne.tran2@ucalgary.ca>
2026-03-19 12:22:05 -0600

    Debug variables' names

--- A4_Q2.py
+++ A4_Q2.py
@@ -6,14 +6,17 @@ from sklearn.metrics import accuracy_score
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split
 
+# Train split training & testing data
 data=load_breast_cancer()
-x=data.data
+X=data.data
 y=data.target
-X_train, X_test, y_train, y_test=train_test_split(x,y, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test=train_test_split(X,y, test_size=0.2, random_state=42)
 
+# Construct the decision tree
 decision_tree_classifier=DecisionTreeClassifier(criterion='entropy')
 decision_tree_classifier.fit(X_train, y_train)
 
+# Predict result and compute accuracy of the model
 predicted_result=decision_tree_classifier.predict(X_test)
 accuracy=accuracy_score(y_test, predicted_result)
 
